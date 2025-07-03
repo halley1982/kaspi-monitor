e7120f6c966552e6ba0c5d0c99ce22121a111d6b
halley1982 <71765787+halley1982@users.noreply.github.com>
2025-07-03 11:01:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-import requests
 import os
+import mysql.connector
 
+# Подключение к базе данных через переменные окружения
 db_config = {
     'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASS'),
     'database': os.getenv('DB_NAME'),
+    'port': 3306
 }
+
+def test_db_connection():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("SELECT NOW()")
+        result = cursor.fetchone()
+        print("Успешное подключение. Серверное время:", result[0])
+        cursor.close()
+        conn.close()
+    except mysql.connector.Error as err:
+        print("Ошибка при подключении:", err)
+
+if __name__ == "__main__":
+    test_db_connection()
+
+
+
 
 url = "https://kaspi.kz/yml/offer-view/offers/102061295"
 
